327ff06fc4b9d14b972c6b280de1ed9b8ab1f3c7
cpcodex <cpcodex23@gmail.com>
2025-01-01 16:27:12 -0500

    add leetcode problem #983

--- LeetCode/LC_983.py
+++ LeetCode/LC_983.py
@@ -49,8 +49,34 @@ Constraints:
 
 """
 
+"""
+a 1-day pass is sold for costs[0] dollars,
+a 7-day pass is sold for costs[1] dollars, and
+a 30-day pass is sold for costs[2] dollars.
+
+For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
+    
+Return the minimum number of dollars you need to travel every day in the given list of days.
+
+Example 1:
+
+Input: days = [1,4,6,7,8,20], costs = [2,7,15]
+Output: 11
+"""
 
 # Start program here
-class Solution:
-    def mincostTickets(self, days: list[int], costs: list[int]) -> int:
-        pass
+# class Solution:
+#     def mincostTickets(self, days: list[int], costs: list[int]) -> int:
+#         pass
+
+
+# days = input()
+# costs = input()
+
+# c1 = Solution()
+# print(c1.mincostTickets(days, costs))
+
+
+# Test start
+days = [1, 4, 6, 7, 8, 20]
+costs = [2, 7, 15]
