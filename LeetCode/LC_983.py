fae231a8e6b6827cc1e029349e05962520b42bbf
cpcodex <cpcodex23@gmail.com>
2025-01-04 21:07:48 -0500

    update solution

--- LeetCode/LC_983.py
+++ LeetCode/LC_983.py
@@ -49,33 +49,6 @@ Constraints:
 
 """
 
-"""
-a 1-day pass is sold for costs[0] dollars,
-a 7-day pass is sold for costs[1] dollars, and
-a 30-day pass is sold for costs[2] dollars.
-
-For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
-    
-Return the minimum number of dollars you need to travel every day in the given list of days.
-
-Example 1:
-
-Input: days = [1,4,6,7,8,20], costs = [2,7,15]
-Output: 11
-"""
-
-"""
-Train tickets are sold in three different ways:
-a 1-day pass is sold for costs[0] dollars,
-a 7-day pass is sold for costs[1] dollars, and
-a 30-day pass is sold for costs[2] dollars.
-
-On day 1, you bought a 1-day pass for costs[0] = $2, which covered day 1.
-On day 3, you bought a 7-day pass for costs[1] = $7, which covered days 3, 4, ..., 9.
-On day 20, you bought a 1-day pass for costs[0] = $2, which covered day 20.
-In total, you spent $11 and covered all the days of your travel.
-"""
-
 
 # Test start
 class Solution:
